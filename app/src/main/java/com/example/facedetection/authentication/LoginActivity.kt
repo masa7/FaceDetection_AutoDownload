c70854d064a9b502a10697dd8d2d313b52dba458
kenkura8 <kurakenz777@gmail.com>
2024-09-21 19:47:19 +0900

    test fonts

--- app/src/main/java/com/example/facedetection/authentication/LoginActivity.kt
+++ app/src/main/java/com/example/facedetection/authentication/LoginActivity.kt
@@ -1,11 +1,8 @@
 package com.example.facedetection.authentication
 
-import android.R
 import android.content.Intent
 import android.graphics.Typeface
 import android.os.Bundle
-import android.view.View
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.facedetection.MainActivity
