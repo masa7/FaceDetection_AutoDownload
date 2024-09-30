44c184ee3f882a920789cdf3ee7a21c53ab266f2
kenkura8 <kurakenz777@gmail.com>
2024-09-30 17:34:38 +0900

    add firestore for registration

--- app/src/main/java/com/example/facedetection/authentication/SignupActivity.kt
+++ app/src/main/java/com/example/facedetection/authentication/SignupActivity.kt
@@ -8,6 +8,8 @@ import com.example.facedetection.R
 import com.example.facedetection.databinding.ActivitySignupBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class SignupActivity : AppCompatActivity() {
 
@@ -39,12 +41,31 @@ class SignupActivity : AppCompatActivity() {
 
     }
 
+    private fun getCurrentDateFormatted(): String {
+        val currentDate = LocalDate.now()
+        val formatter = DateTimeFormatter.ofPattern("yyyyMMdd")
+        return currentDate.format(formatter)
+    }
+
+    private fun sanitizeFirmName(firmName: String): String {
+        // Get the current date in yyyyMMdd format
+        val currentDate = getCurrentDateFormatted()
+
+        // Sanitize firmName
+        return "${firmName.replace("\\s+".toRegex(), "_") // Replace spaces with underscores
+            .replace("[^a-zA-Z0-9_]".toRegex(), "") // Remove special characters
+            .lowercase()}_$currentDate" // Append the current date
+    }
+
     fun signupWithFirebase(userEmail: String, userPassword: String, userName: String, phoneNumber: String, firmName: String, officeAddress: String) {
         auth.createUserWithEmailAndPassword(userEmail, userPassword).addOnCompleteListener { task ->
             if (task.isSuccessful) {
                 // Get the current user
                 val userId = auth.currentUser?.uid
                 Log.i("TAGY", "${userId}")
+
+                // Sanitize firmName for Firestore document ID
+                val sanitizedFirmName = sanitizeFirmName(firmName)
 
                 // Create a HashMap to store user details
                 val userMap = hashMapOf(
@@ -58,7 +79,7 @@ class SignupActivity : AppCompatActivity() {
                 // Save the user details in Firestore
                 userId?.let {
                     firestore.collection("Registration")
-                        .document(userId) // Use the user ID as the document ID
+                        .document(sanitizedFirmName)
                         .set(userMap)
                         .addOnSuccessListener {
                             Toast.makeText(applicationContext, "Account has been created and data saved", Toast.LENGTH_SHORT).show()
